4b396ada45eae262ddd9548e71b2c68662163a54
ashvita16 <ashvita0000gmail.com>
2021-09-16 17:29:33 +0530

    Second Commit :Added jUnit library

--- MavenWeb2/src/test/java/MyCalculatorTest.java
+++ MavenWeb2/src/test/java/MyCalculatorTest.java
@@ -1,7 +1,8 @@
 import static org.junit.Assert.*;
-
-import org.junit.Test;
 import com.jenk.MyCalculator;
+import org.junit.Test;
+
+
 public class MyCalculatorTest {
 
 
@@ -12,11 +13,11 @@ public class MyCalculatorTest {
 	
 	@Test
 	public void test2() {
-		assertEquals(10,new MyCalculator().getSum(2,3));
+		assertEquals(5,new MyCalculator().getSum(2,3));
 	}
 	
 	@Test
 	public void test3() {
-		assertEquals(10,new MyCalculator().getDiff(10,5));
+		assertEquals(5,new MyCalculator().getDiff(10,5));
 	}
 }
